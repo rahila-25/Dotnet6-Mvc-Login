c5e1c0f01d779c14c22f76bf6992afec0990d0e3
minhaj qureshi <118585799+minhajqureshi60@users.noreply.github.com>
2023-11-03 14:42:58 +0530

    Update install_dependencies.sh

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -3,6 +3,6 @@
 # Install any dependencies required by the .NET application
 
 # Example: Install .NET runtime
-#sudo apt update
-#sudo apt install -y dotnet-runtime-6.0
-#sudo apt install -y dotnet-sdk-6.0
+sudo apt update
+sudo apt install -y dotnet-runtime-6.0
+sudo apt install -y dotnet-sdk-6.0
